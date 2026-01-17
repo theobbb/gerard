f9a1b5a5d371299c12a29dfa337a37089d4af89b
Theo <theobbb64@gmail.com>
2026-01-17 15:21:20 -0500

    g

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,8 @@ import { pocketbase } from '$lib/pocketbase';
 import type { ItemsRecord } from '$lib/pocketbase.types';
 import type { PaginationResult } from '$lib/types';
 
+export const prerender = true;
+
 export async function load() {
 	const pagination: PaginationResult<ItemsRecord> = await pocketbase
 		.collection('items')
